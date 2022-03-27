1611f8c209abd63ac90fc32666d66f4717dca906
shanurrahman <shanur.cse.nitap@gmail.com>
2022-03-27 19:02:54 +0000

    pulumi to quickly create and deploy a static website to aws

--- s3/index.ts
+++ s3/index.ts
@@ -1,9 +1,28 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import aws = require("@pulumi/aws");
+import pulumi = require("@pulumi/pulumi");
+import mime = require("mime");
+import { publicReadPolicyForBucket } from "./policies/public_read";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// Create an S3 bucket
+let siteBucket = new aws.s3.Bucket("s3-website-bucket");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+let siteDir = "www"; // directory for content files
+
+// For each file in the directory, create an S3 object stored in `siteBucket`
+for (let item of require("fs").readdirSync(siteDir)) {
+    let filePath = require("path").join(siteDir, item);
+    let object = new aws.s3.BucketObject(item, {
+      bucket: siteBucket,
+      source: new pulumi.asset.FileAsset(filePath),     // use FileAsset to point to a file
+      contentType: mime.getType(filePath) || undefined, // set the MIME type of the file
+    });
+}
+
+
+let bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: siteBucket.bucket,
+    policy: siteBucket.bucket.apply(publicReadPolicyForBucket)
+  });
+  
+exports.websiteUrl = siteBucket.websiteEndpoint; // output the endpoint as a stack output  
+exports.bucketName = siteBucket.bucket; 
---
+++ s3/policies/public_read.ts
@@ -0,0 +1,15 @@
+export const publicReadPolicyForBucket = (bucketName: any) => {
+    return JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Principal: "*",
+            Action: [
+                "s3:GetObject"
+            ],
+            Resource: [
+                `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
+            ]
+        }]
+    })
+}
